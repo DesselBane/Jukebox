4ef862958eefb11fcebfe4aa8d6ca4e116c11ffd
Dessel Bane <dessel.bane@gmx.de>
2018-02-26 16:09:15 +0100

    Added Song TS Model (not final)

--- jukebox/src/app/player/models/song.ts
+++ jukebox/src/app/player/models/song.ts
@@ -3,6 +3,7 @@ export class Song {
   public title: string;
   public artist: string;
   public album: string;
+  public url: string;
 
   constructor(title: string, artist?: string, album?: string)
   {
