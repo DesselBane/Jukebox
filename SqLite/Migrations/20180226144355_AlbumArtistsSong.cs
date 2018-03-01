f624196b13f246812468054227baf6261f92cc2d
Dessel Bane <dessel.bane@gmx.de>
2018-03-01 13:15:10 +0100

    Added Migrations (idk why I excluded them...)

---
+++ SqLite/Migrations/20180226144355_AlbumArtistsSong.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Jukebox.Database.SqLite.Migrations
+{
+    public partial class AlbumArtistsSong : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                                               "Album",
+                                               "Songs",
+                                               nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                                               "ArtistsDb",
+                                               "Songs",
+                                               nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                                        "Album",
+                                        "Songs");
+
+            migrationBuilder.DropColumn(
+                                        "ArtistsDb",
+                                        "Songs");
+        }
+    }
+}
---
+++ SqLite/Migrations/20180227111845_AddedSongLastTimeIndexed.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Jukebox.Database.SqLite.Migrations
+{
+    public partial class AddedSongLastTimeIndexed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastTimeIndexed",
+                table: "Songs",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastTimeIndexed",
+                table: "Songs");
+        }
+    }
+}
