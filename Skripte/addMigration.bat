b7585708eb3b8026a47a3cb6fe4072bfa39d5230
D4rth B4n3 <dessel.bane@gmx.de>
2018-03-29 15:58:04 +0200

    updated addMigration.bat

--- Skripte/addMigration.bat
+++ Skripte/addMigration.bat
@@ -4,5 +4,5 @@ set migrationName=%1
 
 cd ..\JukeboxAPI
 call dotnet build
-call dotnet ef migrations add %migrationName% -p ..\SqLite\SqLite.csproj --no-build
+call dotnet ef migrations add %migrationName% -p ..\Jukebox.Data.SqLite\Jukebox.Data.SqLite.csproj --no-build
 cd ..\Skripte
