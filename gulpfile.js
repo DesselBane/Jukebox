06d4356c7e33f84bcd4858845d93c37c5d4ceb11
D4rth B4n3 <dessel.bane@gmx.de>
2018-04-26 15:24:02 +0200

    implemented #24

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ let exec = require('gulp-exec');
 
 // Genral stuff
 
-gulp.task('clean-all', ['clean-electron-angular', 'clean-api-angular', 'clean-win32', 'clean-win64'], () => {
+gulp.task('clean-all', ['clean-electron-angular', 'clean-api-angular', 'clean-win32', 'clean-win64', 'clean-linux'], () => {
   console.log('All clean now!');
 });
 
@@ -25,6 +25,42 @@ gulp.task('build-api-angular', () => {
   return gulp.src(__dirname + '/jukebox')
     .pipe(exec(`ng build --op=${__dirname}/JukeboxAPI/wwwroot`));
 });
+
+// Linux Build
+gulp.task('build-linux', ['build-api-linux', 'copy-electron-linux', 'copy-package-json-linux', 'copy-angular-linux'], () => {
+  console.log('Linux build complete');
+});
+
+gulp.task('clean-linux', () => {
+  return del(__dirname + '/out/linux');
+});
+
+gulp.task('copy-prebuilts-linux', ['clean-linux'], () => {
+  return gulp.src(__dirname + '/prebuilt/linux/**/*')
+    .pipe(gulp.dest(__dirname + '/out/linux/'));
+});
+
+gulp.task('build-api-linux', ['build-api-angular', 'copy-prebuilts-linux'], () => {
+  return gulp.src(__dirname)
+    .pipe(exec(`cd JukeboxAPI && dotnet publish -r linux-x64 -c Release -o ${__dirname}/out/linux/resources/app/api/linux/`));
+});
+
+gulp.task('copy-electron-linux', ['copy-prebuilts-linux'], () => {
+  return gulp.src(__dirname + '/jukebox/main.js')
+    .pipe(gulp.dest(__dirname + '/out/linux/resources/app/'))
+});
+
+gulp.task('copy-package-json-linux', ['copy-prebuilts-linux'], () => {
+  return gulp.src(__dirname + '/package.json')
+    .pipe(gulp.dest(__dirname + '/out/linux/resources/app/'));
+
+});
+
+gulp.task('copy-angular-linux', ['build-electron-angular', 'copy-prebuilts-linux'], () => {
+  return gulp.src(__dirname + '/jukebox/dist/**/*')
+    .pipe(gulp.dest(__dirname + '/out/linux/resources/app/dist/'));
+});
+
 
 // Win 32 Build
 gulp.task('build-win32', ['build-api-win32', 'copy-electron-win32', 'copy-package-json-win32', 'copy-angular-win32'], () => {
@@ -71,7 +107,6 @@ gulp.task('clean-win64', () => {
   return del(__dirname + '/out/win64');
 });
 
-
 gulp.task('copy-prebuilts-win64', ['clean-win64'], () => {
   return gulp.src(__dirname + '/prebuilt/win64/**/*')
     .pipe(gulp.dest(__dirname + '/out/win64/'));
@@ -101,6 +136,6 @@ gulp.task('copy-angular-win64', ['build-electron-angular', 'copy-prebuilts-win64
 
 // Default Task
 
-gulp.task('default', ['build-win64', 'build-win32'], () => {
+gulp.task('default', ['build-win64', 'build-win32', 'build-linux'], () => {
 
 });
