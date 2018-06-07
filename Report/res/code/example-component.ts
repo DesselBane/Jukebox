a995bec06664b25dd700e820aeb3f99d98af5e29
D4rth B4n3 <dessel.bane@gmx.de>
2018-06-07 13:26:14 +0200

    started to describe ng components

---
+++ Report/res/code/example-component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-nav-item', |\label{line:comp_selector}|
+  templateUrl: './nav-item.component.html', |\label{line:comp_templateUrl}|
+  styleUrls: ['./nav-item.component.css'] |\label{line:comp_styleUrls}|
+})
+export class NavItemComponent implements OnInit {
+  constructor() { }
+  ngOnInit() {  }
+}
