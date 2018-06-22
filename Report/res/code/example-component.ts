3e303ae91ff9827bd826c99082f6a0dd548958c2
D4rth B4n3 <dessel.bane@gmx.de>
2018-06-22 14:12:57 +0200

    finished Template Syntax Subsubsection

--- Report/res/code/example-component.ts
+++ Report/res/code/example-component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AngularMenuItem} from "../models/angular-menu-item";
 
 @Component({
   selector: 'app-nav-item', |\label{line:comp_selector}|
@@ -6,6 +7,12 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./nav-item.component.css'] |\label{line:comp_styleUrls}|
 })
 export class NavItemComponent implements OnInit { |\label{line:comp_OnInit}|
+
+  @Input() |\label{line:comp_input}|
+  CurrentItem: AngularMenuItem;
+  @Output() |\label{line:comp_output}|
+  ItemClicked = new EventEmitter();
+
   constructor() { }
   ngOnInit() {  } |\label{line:comp_ngOnInit}|
 }
